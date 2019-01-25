156c97772378fffec1de24e7529920ba75bc850f
pranav <pranavverma550@gmail.com>
2019-01-25 20:19:53 +0530

    third commit

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const users = express.Router();
-const cors = require("cors");
+var express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const router = express.Router();
 const User = require("../models/Users");
-users.use(cors());
+const cors = require("cors");
+router.use(cors());
 
 process.env.SECRET_KEY = "secret";
 
-users.post("/register", (req, res) => {
+router.post("/register", (req, res) => {
   const today = new Date();
   const userData = {
     first_name: req.body.first_name,
-    last_name: req.body.last_name,
+    last_name: req.body.first_name,
     email: req.body.email,
     password: req.body.password,
     created: today
@@ -25,21 +25,68 @@ users.post("/register", (req, res) => {
       if (!user) {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
           userData.password = hash;
+
           User.create(userData)
-            .then(user => {
-              res.json({ status: user.email + "Registered" });
-            })
-            .catch(err => {
-              res.send("error" + err);
-            });
+            .then(() => res.json({ msg: "User Sucessfully Registered" }))
+            .catch(err => res.json({ msg: "error:" + err }));
         });
       } else {
-        res.json({ error: "User already Exist" });
+        res.json({ error: "User Already Exists" });
       }
     })
     .catch(err => {
-      res.send("error" + err);
+      res.json({ msg: "error " + err });
     });
 });
 
-module.exports = users;
+router.post("/login", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      if (user) {
+        if (bcrypt.compare(req.body.password, user.password)) {
+          const payload = {
+            _id: user._id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+          };
+          let token = jwt.sign(payload, process.env.SECRET_KEY, {
+            expiresIn: 1440
+          });
+          res.json({ token: token });
+        } else {
+          res.json({ error: "User Does Not Exist" });
+        }
+      } else {
+        res.json({ error: "User Does Not Exist" });
+      }
+    })
+    .catch(err => {
+      res.json({ error: +err });
+    });
+});
+
+router.get("/profile", (req, res) => {
+  var decoded = jwt.verify(
+    req.headers["authorization"],
+    process.env.SECRET_KEY
+  );
+
+  User.findOne({
+    _id: decoded._id
+  })
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.json({ err: "User Does Not Exist " });
+      }
+    })
+    .catch(err => {
+      res.json({ err: "Error " + err });
+    });
+});
+
+module.exports = router;
